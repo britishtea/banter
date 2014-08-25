ea8bdbb9105b33272bb5448e82203057d08b6311
Jip van Reijsen <jipvanreijsen@gmail.com>
2014-08-26 01:20:03 +0200

    Introduce Banter::Plugin

---
+++ lib/banter/plugin.rb
@@ -0,0 +1,96 @@
+require "irc/rfc2812/commands"
+require "irc/rfc2812/constants"
+
+module Banter
+  MissingSettings = Class.new KeyError
+
+  # Public: Represents a plugin.
+  class Plugin
+    include IRC::RFC2812::Commands
+    include IRC::RFC2812::Constants
+
+    def self.name;  @name;  end
+    def self.usage; @usage; end
+
+    def self.define(name, usage = nil, &block)
+      @name, @usage, @block = name, usage, block
+    end
+
+    # Public: Creates a new plugin and calls it.
+    def self.call(event, network, *args)
+      new(event, network, *args, &@block).call
+    end
+
+    # Public: Gets the Banter::Network.
+    attr_reader :network
+
+    # Public: Initializes the plugin.
+    def initialize(event, network, *args, &block)
+      @event, @network, @args, @block = event, network, args, block
+    end
+
+    # Public: Gets the plugin settings ThreadSafe::Hash.
+    def settings
+      self.network.settings[self.class]
+    end
+
+    # Public: Guarantees settings are present.
+    #
+    # keys - Key Symbols.
+    #
+    # Examples
+    #
+    #   required :api_key, :api_secret
+    #
+    # Raises Banter::MissingSettings if one or more settings are not present.
+    def required(*keys)
+      missing = keys.reject { |key| self.settings.key? key }
+      
+      unless missing.empty?
+        raise MissingSettings, "The setting(s) #{missing.join ","} are not set "
+                               "for #{self.class}"
+      end
+
+      return true
+    end
+
+    # Public: Sets default settings if settings are not present.
+    #
+    # defaults - A Hash.
+    #
+    # Examples
+    #
+    #   default :nickname => "banter", :username => "banter"
+    def default(defaults)
+      defaults.each do |key, value|
+        self.settings[key] = value unless self.settings.key? key
+      end
+    end
+
+    # Public: Runs `block` if `event` matches the event.
+    #
+    # event - An event Symbol.
+    #
+    # Examples
+    #
+    #   event :receive do |message|
+    #     "..."
+    #   end
+    def event(event, &block)
+      block.call(*@args) if @event == event
+    end
+
+    # Public: Executes the plugin.
+    def call
+      instance_exec *@args, &@block
+    end
+
+    # Public: Pushes messages onto the network queue. This method is also used
+    # by the IRC::RFC2812::Commands mixin.
+    #
+    # message - A message String.
+    def raw(message)
+      self.network.queue.push message.to_s
+    end
+  end
+end
---
+++ test/banter/plugin.rb
@@ -0,0 +1,104 @@
+require_relative "../test_helper"
+require "banter/plugin"
+require "banter/network"
+
+setup { Banter::Plugin }
+
+prepare do
+  $network        = Banter::Network.new "irc://0.0.0.0:6667"
+  $implementation = proc { |*args| $test = args }
+  $test           = nil
+end
+
+test "defining a plugin" do |plugin|
+  plugin.define "name", &$implementation
+end
+
+test "defining a plugin with a usage String" do |plugin|
+  plugin.define "name", "usage", &$implementation
+end
+
+test "getting the plugin name" do |plugin|
+  plugin.define "name", &$implementation
+
+  assert_equal plugin.name, "name"
+end
+
+test "getting the usage String from a plugin without usage String" do |plugin|
+  plugin.define "name", &$implementation
+
+  assert_equal plugin.usage, nil
+end
+
+test "getting the usage String from a plugin with usage String" do |plugin|
+  plugin.define "name", "usage", &$implementation
+
+  assert_equal plugin.usage, "usage"
+end
+
+test "executing a plugin" do |plugin|
+  plugin.define "name", "usage", &$implementation
+  plugin.call :event, "network", "message"
+
+  assert_equal $test, ["message"]
+end
+
+# Convenience methods
+
+setup { Banter::Plugin.new :event, $network, "args" }
+
+test "getting the network" do |plugin|
+  assert_equal plugin.network, $network
+end
+
+test "getting the settings" do |plugin|
+  assert plugin.settings.equal?($network.settings[plugin.class])
+end
+
+test "required settings that are not set" do |plugin|
+  plugin.settings[:one] = "one"
+
+  assert_raise(Banter::MissingSettings) { plugin.required :one, :two }
+end
+
+test "required settings that are set" do |plugin|
+  plugin.settings[:one] = "one"
+  plugin.settings[:two] = "two"
+
+  assert plugin.required :one, :two
+end
+
+test "default settings that are not set" do |plugin|
+  plugin.default :one => "one", :two => "two"
+
+  assert_equal plugin.settings.values_at(:one, :two), ["one", "two"]
+end
+
+test "default settings that are set" do |plugin|
+  plugin.settings[:one], plugin.settings[:two] = "one", "two"
+  plugin.default :one => "ONE", :two => "TWO"
+
+  assert_equal plugin.settings.values_at(:one, :two), ["one", "two"]
+end
+
+test "running a block per event" do |plugin|
+  plugin.event(:event)       { |*args| $test = [:event, args]  }
+  plugin.event(:other_event) { |*args| $test = [:other_event, args] }
+
+  assert_equal $test, [:event, ["args"]] 
+end
+
+test "working with irc-helpers" do |plugin|
+  commands_methods = IRC::RFC2812::Commands.instance_methods false
+  plugin_methods   = Banter::Plugin.instance_methods false
+
+  assert_equal commands_methods - plugin_methods, commands_methods - [:raw]
+end
+
+test "sending messages to the network" do |plugin|
+  plugin.raw "hello\nbye\n"
+
+  assert_equal $network.queue.pop, "hello\nbye\n"
+end
+
+# TODO: cuba style on matchers (?), #reply, User and Channel helpers.
